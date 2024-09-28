7ceefd5394d8eac7243a781f2b7f8a9928a4d28d
sethk3 <threegartz@gmail.com>
2024-09-28 16:29:46 -0600

    initialized git in an old folder, reinstating this as new repo

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import { useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { images } from "../constants"
-// import CustomButton from '../components/CustomButton';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   const router = useRouter();
@@ -14,6 +14,8 @@ export default function App() {
       <View className="w-full justify-center items-center min-h-[85vh] px-4">
         <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode='contain' backgroundColor="#000000"/>
         <Text className="text-3xl text-white font-bold text-center">Welcome to MatchMatch</Text>
+      
+      <CustomButton title="Continue with Email" handlePress={() => router.push('/sign-in')} containerStyles="w-full mt-7"/>
       </View>
       
       </ScrollView>
