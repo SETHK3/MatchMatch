b9b08be1c69a0e8a936090ad9af89a1f9dbea740
sethk3 <threegartz@gmail.com>
2024-09-29 22:13:21 -0600

    additonal stylings on the custom button and sign in page

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -25,7 +25,7 @@ const SignIn = () => {
         <View className="w-full justify-center min-h-[75vh] px-4 my-6">
           <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]"/>
 
-          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to MatchMatch</Text>
+          <Text className="text-2xl text-secondary text-semibold mt-10 font-psemibold">Log in to MatchMatch</Text>
 
           <FormField 
             title="Email"
@@ -51,7 +51,7 @@ const SignIn = () => {
             <Text className="text-lg text-gray-100 font-pregular">
               Don't have an account?
             </Text>
-            <Link href="/sign-up" className='text-lg font-psemibold text-secondary'>Sign Up</Link>
+            <Link href="/sign-up" className='text-lg font-psemibold text-white'>Sign Up</Link>
           </View>
         </View>
       </ScrollView>
--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -6,9 +6,9 @@ const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoadi
     <TouchableOpacity 
         onPress={handlePress}
         activeOpacity={0.7}
-        className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} {isLoading ? 'opacity-50' : ''}`}
+        className={`bg-white rounded-xl min-h-[62px] justify-center items-center ${containerStyles} {isLoading ? 'opacity-50' : ''}`}
         disabled={isLoading}>
-      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>{title}</Text>
+      <Text className={`text-primary font-psemibold text-lg text-primary ${textStyles}`}>{title}</Text>
     </TouchableOpacity>
   )
 }
